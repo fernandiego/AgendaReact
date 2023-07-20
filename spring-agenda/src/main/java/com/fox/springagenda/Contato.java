0928508ebfefc18b8de4f6f13c323b19b537c583
fox <fernandodiego.sn@gmail.com>
2023-07-20 14:01:22 -0300

    Salvando os dados no banco H2, próximo passo integrar com o frontend. Agenda  React

--- spring-agenda/src/main/java/com/fox/springagenda/Contato.java
+++ spring-agenda/src/main/java/com/fox/springagenda/Contato.java
@@ -1,15 +1,19 @@
 package com.fox.springagenda;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Entity
 @Table(name="contatos")
+@Data
+@NoArgsConstructor
 public class Contato {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long contatoId;
     String nome;
     String telefone;
@@ -23,38 +27,4 @@ public class Contato {
         this.email = email;
     }
 
-    public Contato() {
-    }
-
-    public Long getId() {
-        return contatoId;
-    }
-
-    public void setId(Long id) {
-        this.contatoId = contatoId;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- spring-agenda/src/main/java/com/fox/springagenda/SpringAgendaApplication.java
+++ spring-agenda/src/main/java/com/fox/springagenda/SpringAgendaApplication.java
@@ -2,13 +2,25 @@ package com.fox.springagenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringAgendaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAgendaApplication.class, args);
-		System.out.println("servidor");
+		ApplicationContext ctx = SpringApplication.run(SpringAgendaApplication.class, args);
+
+		ContatoService contatoService = ctx.getBean(ContatoService.class);
+
+		Contato contato = new Contato();
+		contato.nome = "Nome";
+		contato.telefone = "85999154477";
+		contato.email = "fox@gmail.com";
+
+
+
+		System.out.println(contato);
+		contatoService.salvaContato(contato);
 	}
 
 }
